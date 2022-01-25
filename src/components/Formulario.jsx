4889416285b01faaf184f759155bb39e05e5dd80
Franco Campaiola <francocampaiola@hotmail.com>
2022-01-25 00:19:10 -0300

    :+1: Se realiza la consulta y carga de datos a partir de la API de CryptoCompare

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import useSelectMonedas from '../hooks/useSelectMonedas';
 import { monedas } from '../data/monedas.js'
+import { useEffect, useState } from 'react';
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
@@ -23,13 +24,37 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
+    const [criptos, setCriptos] = useState([]);
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas)
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda:', monedas)
+    const [criptomoneda, SelectCriptomoneda] = useSelectMonedas('Elige tu criptomoneda:', criptos)
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const arrayCripto = resultado.Data.map( cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto;
+            })
+
+            setCriptos(arrayCripto);
+        }
+        consultarAPI();
+    }, []);
+    
 
     return (
         <form>
 
             <SelectMonedas />
+
+            <SelectCriptomoneda />
 
             <InputSubmit
                 type="submit"
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 import styled from '@emotion/styled';
 
 const Label = styled.label`
     color: #FFF;
+    display: block;
+    width: 100%;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 5px;
+    border-radius: 5px;
+    margin-bottom: 20px;
 `
 
 const useSelectMonedas = (label, opciones) => {
+
+    const [state, setState] = useState('')
+
     const SelectMonedas = () => (
         <>
             <Label>{label}</Label>
-            <select>
+            <Select
+                value={state}
+                onChange={ e => setState(e.target.value)}
+            >
                 <option value="Seleccione"></option>
                     {opciones.map ( opcion => (
                         <option
@@ -17,11 +37,11 @@ const useSelectMonedas = (label, opciones) => {
                             value={opcion.id}
                         > {opcion.nombre} </option>
                     ))}
-            </select>
+            </Select>
         </>
     )
-    
-    return [ SelectMonedas ]
+
+    return [ state, SelectMonedas ]
 }
 
 export default useSelectMonedas;
